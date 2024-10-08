e21d698fffcf2a7e66f136e906695ec0d8d58f88
akmuola <akinroyemo@gmail.com>
2024-10-08 10:40:37 +0100

    Updated

--- src/components/CallOfDuty.jsx
+++ src/components/CallOfDuty.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Tournament from './Tournament'; // Assuming you want to display tournaments here
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 const CallOfDuty = () => {
-  return (
-    <div>
-      <p style={{marginLeft:'10px', cursor:'pointer'}}><img src={`${process.env.PUBLIC_URL}/assets/left.png`} alt="Trophy Icon" style={{ height: '20px' }} />Back</p>
-      <Tournament />
-      {/* Add more content specific to Call of Duty if needed */}
-    </div>
-  );
+    const navigate = useNavigate(); // Replace useHistory with useNavigate
+
+    const handleNavigation = () => {
+        navigate('/'); // Redirects to the homepage
+    };
+
+    return (
+        <div>
+            <p style={{ marginLeft: '10px', cursor: 'pointer' }} onClick={handleNavigation}>
+                <img src={`${process.env.PUBLIC_URL}/assets/left.png`} alt="Trophy Icon" style={{ height: '20px' }} />Back
+            </p>
+            <Tournament />
+            {/* Add more content specific to Call of Duty if needed */}
+        </div>
+    );
 };
 
 export default CallOfDuty;
